c7c6c15466ef77a0642f24d14ca6d80d2a5a216d
FelipeMMoraes <felipe.moraes8@hotmail.com>
2022-12-22 11:24:59 -0300

    feat: routes

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,14 @@ import GlobalStyle from './styles/global';
 
 import theme from './styles/theme';
 
-import { New } from './pages/New';
+import { Routes } from './routes';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <New />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>,
 )
