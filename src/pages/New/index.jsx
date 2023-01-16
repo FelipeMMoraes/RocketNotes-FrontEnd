10032a72966b365bc0eae6284dbcd8ccdaccce7e
FelipeMMoraes <felipe.moraes8@hotmail.com>
2023-01-16 16:49:14 -0300

    feat: function remove tags

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -31,6 +31,10 @@ export function New() {
     setNewTag("")
   }
 
+  function handleRemoveTag(deleted) {
+    setTags(prevState => prevState.filter(tag => tag !== deleted));
+  }
+
   return (
     <Container>
       <Header />
@@ -44,11 +48,11 @@ export function New() {
 
           <Input 
           placeholder="Título"
-          onChange={e => setTitle(e.target.value)}
+          // onChange={e => setTitle(e.target.value)}
           />
           <Textarea
           placeholder="Observações"
-          onChange={e => setDescription(e.target.value)}
+          // onChange={e => setDescription(e.target.value)}
           />
 
           <Section title="Links Úteis">
@@ -77,7 +81,7 @@ export function New() {
                   <NoteItem 
                   key={String(index)}
                   value={tag} 
-                  onClick={() => {}}
+                  onClick={() => handleRemoveTag(tag)}
                   />
                 ))
               }
@@ -94,7 +98,6 @@ export function New() {
 
           <Button
             title="Salvar"
-            onClick={handleNewNote}
           />
         </Form>
       </main>
