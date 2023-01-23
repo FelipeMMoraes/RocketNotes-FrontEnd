b7ddc774b05d2709d54feb854f7a48bdd71e4eda
FelipeMMoraes <felipe.moraes8@hotmail.com>
2023-01-23 18:10:38 -0300

    feat: search notes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -85,7 +85,7 @@ export function Home() {
       <Search>
         <Input
           placeholder="Pesquisar pelo título"
-          onChange={() => setSearch(e.target.value)}
+          onChange={(e) => setSearch(e.target.value)}
           />
       </Search>
 
