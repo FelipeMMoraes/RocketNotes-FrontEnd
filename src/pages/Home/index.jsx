16caeaa17ca1f3c5755502236bd091ecacccb5a7
FelipeMMoraes <felipe.moraes8@hotmail.com>
2023-06-24 21:29:16 -0300

    feat: adjust menu mobile

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,11 +3,11 @@ import { useNavigate } from 'react-router-dom'
 import { FiPlus } from 'react-icons/fi';
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
 
+import { ButtonText } from '../../components/ButtonText';
+import { Section } from '../../components/Section';
+import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
 import { Note } from '../../components/Note';
-import { Header } from '../../components/Header';
-import { Section } from '../../components/Section';
-import { ButtonText } from '../../components/ButtonText';
 
 import { api } from '../../services/api';
 
